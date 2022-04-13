9f396abb85fb95f40118bca1d85ca626af1cc088
baltej223 <73211864+baltej223@users.noreply.github.com>
2022-04-13 19:30:36 +0530

    Update b.js

--- b.js
+++ b.js
@@ -1,10 +1,35 @@
+Object.defineProperty(String.prototype, "id", {
+  value() {
+    if (this.slice(0,1).includes("#")){
+     return true;
+    }
+    else{
+      return false;
+    }
+  },
+  writable: true,
+  configurable: true,
+});
+
+Object.defineProperty(String.prototype, "class", {
+  value() {
+    if (this.slice(0,1).includes(".")){
+     return true;
+    }
+    else{
+      return false;
+    }
+  },
+  writable: true,
+  configurable: true,
+});
 function get(x,y){
-if(x.slice(0,1).includes("#")){
+if(x.id()){
   //an id
   x = x.replace("#","");
    return document.getElementById(x);
 }
-else if(x.slice(0,1).includes(".")){
+ if(x.class()){
   //its a class
   x = x.replace(".","");
   if (y != null){
@@ -17,18 +42,16 @@ else {
      return document.getElementsByTagName(x)[y];
    }
    else{
-    var fwe = document.getElementsByTagName(x);
-    for (let i = 0; i < fwe.length; i++) {
-      return fwe[i];
+    console.log('index not provided');
    }
  }
 }
-}
+
 function call(call_funcion){
 setTimeout(call_funcion,1);
 }
-function hide(idOrClass,Index){
-if (a.split(0,1).includes(".")){
+function hide(ids,Index){
+if (ids.class()){
   if (index != null | undefined){
     return document.getElementsByClassName(idOrClass)[Index].style.display="none"; 
   }
@@ -36,7 +59,7 @@ if (a.split(0,1).includes(".")){
     console.error("index of clas not provided at hide()");
   }
 }
-if(a.split(0,1).includes("#")){
+if(ids.id()){
   return document.getElementById(idOrClass).style.display="none";
 }
 }
@@ -48,22 +71,22 @@ function del(del){
 }
 function createElement(element,iClass){
   var ele = document.createElement(element);
-  if (iClass.slice(0,1).includes(".")){
+  if (iClass.class()){
     ele.setAttribute("class",iClass.replace(".",""));
     document.body.appendChild(ele);    
   }
-  if (iClass.slice(0,1).includes("#")){
+  if (iClass.id()){
     ele.setAttribute("id",iClass.replace("#",""));
     document.body.appendChild(ele);
   }
 }
 function print(x,y,z) {
   //usage print("what to write" , "<id>|class" ,"index if class");
-  if (y.slice(0,1).includes("#")){
+  if (y.id()){
     y = y.replace("#","");
     document.getElementById(y).innerHTML=x;
   }
-  if (y.slice(0,1).includes(".")){
+  if (y.class()){
     y = y.replace(".","");
     document.getElementsByClassName(y)[z].innerHTML=x;}
 if(z === undefined){ 
@@ -97,17 +120,46 @@ Object.defineProperty(Element.prototype, "unhide", {
   writable: true,
   configurable: true,
 });
-function replaceAll(from,what,wf){
-  var aaa = from.split(what);
-  return aaa.join(wf);
-}
-function onresize(fun){document.body.addEventListener(onresize,function(){call(fun);});}
 
+window.onresize = resize;
+function resize(func)
+{
+ call(func);
+}
 Object.defineProperty(Element.prototype, "center", {
   value() {
     this.style.marginLeft='auto';
     this.style.marginRight='auto';
     this.style.textAlign = "center";
+  },
+  writable: true,
+  configurable: true,
+});
+Object.defineProperty(Element.prototype, "left", {
+  value() {
+    this.style.float='left';
+  },
+  writable: true,
+  configurable: true,
+});
+Object.defineProperty(Element.prototype, "right", {
+  value() {
+    this.style.float='right';
+  },
+  writable: true,
+  configurable: true,
+});
+Object.defineProperty(String.prototype, "replaceAll", {
+  value(what,wf) {
+    let aaa = this.split(what);
+    return aaa.join(wf);
+  },
+  writable: true,
+  configurable: true,
+});
+Object.defineProperty(Element.prototype, "print", {
+  value(thD) {
+    this.innerHTML=thD;
   },
   writable: true,
   configurable: true,
